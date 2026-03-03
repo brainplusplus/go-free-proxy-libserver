c83a878f1bd69d2c776b657e911c03462298c168
Muhammad Tri Wibowo <akhmad.mib@gmail.com>
2026-03-03 21:59:56 +0700

    feat: add proxy get and list handlers

---
+++ server/handler.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	freeproxy "github.com/yourusername/freeproxy"
+)
+
+// ProxyResponse wraps a single proxy result.
+type ProxyResponse struct {
+	Data  *freeproxy.FreeProxy `json:"data,omitempty"`
+	Error string               `json:"error,omitempty"`
+}
+
+// ProxyListResponse wraps a list of proxies.
+type ProxyListResponse struct {
+	Data  []freeproxy.FreeProxy `json:"data"`
+	Total int                   `json:"total"`
+	Error string                `json:"error,omitempty"`
+}
+
+func getProxyHandler(c *fiber.Ctx) error {
+	param := freeproxy.FreeProxyParameter{
+		CategoryCode: c.Query("category_code"),
+		TargetUrl:    c.Query("target_url"),
+	}
+
+	proxy, err := freeproxy.GetProxy(param)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(ProxyResponse{
+			Error: err.Error(),
+		})
+	}
+
+	return c.JSON(ProxyResponse{Data: proxy})
+}
+
+func getProxyListHandler(c *fiber.Ctx) error {
+	param := freeproxy.FreeProxyParameter{
+		CategoryCode: c.Query("category_code"),
+		TargetUrl:    c.Query("target_url"),
+	}
+
+	list, err := freeproxy.GetProxyList(param)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(ProxyListResponse{
+			Data:  []freeproxy.FreeProxy{},
+			Total: 0,
+			Error: err.Error(),
+		})
+	}
+
+	if list == nil {
+		list = []freeproxy.FreeProxy{}
+	}
+
+	return c.JSON(ProxyListResponse{
+		Data:  list,
+		Total: len(list),
+	})
+}
